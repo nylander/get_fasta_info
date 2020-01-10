a9a774275ca2fdc544b7e177d7824d6a3f2d3e83
nylander <j.a.a.nylander@gmail.com>
2020-01-10 16:24:39 +0100

    start fastq branch

---
+++ src/get_fastq_info.c
@@ -0,0 +1,152 @@
+/*        
+*          File: get_fastq_info.c
+*            By: Johan Nylander
+* Last modified: fre jan 10, 2020  04:22
+*   Description: Get min/max/avg sequence length in fastq.
+*                Can read compressed (gzip) files.
+*                Prints to both stdout and stderr.
+*       Compile: gcc -Wall -o get_fastq_info get_fastq_info.c -lm -lz
+*           Run: get_fastq_info fastq.fq.gz
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+#include <libgen.h>
+#include <math.h>
+#include <zlib.h>
+
+int main (int argc, char **argv) {
+
+    long int minlen;
+    long int maxlen;
+    long int sum;
+    long int seqlen;
+    long int nseqs;
+    long int linecounter;
+    float meanlen;
+    char r; // r is the character currently read
+    int inheader;
+    char *fname;
+    extern char *optarg;
+    extern int optind;
+    int c, err = 0;
+
+    static char usage[] = "Usage: %s [-h] infile(s)\n";
+
+    if (argc == 1) {
+        fprintf(stderr, usage, argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    while ((c = getopt(argc, argv, "h")) != -1) {
+        switch (c) {
+            case 'h':
+                fprintf(stderr, usage, argv[0]);
+                exit(1);
+                break;
+            case '?':
+                err = 1;
+                break;
+        }
+    }
+
+    if (err) {
+        fprintf(stderr, usage, argv[0]);
+        exit(1);
+    }
+
+    if (optind < argc) {
+
+        for (; optind < argc; optind++) {
+
+            gzFile zfp = gzopen(argv[optind], "rb");
+
+            if (zfp == NULL) {
+                perror("Error: failed in opening file");
+                exit(EXIT_FAILURE);
+            }
+
+            fname = argv[optind];
+            minlen = INT_MAX;
+            maxlen = INT_MIN;
+            inheader = 0;
+            nseqs = 0;
+            seqlen = 0;
+            sum = 0;
+
+            while ((r = gzgetc(zfp)) != EOF) {
+
+            // Initialize line counter to 0;
+            // read character by character;
+            // if the character is a '\n', then
+            //   increment line counter
+            // if the line counter == 1, then
+            //   add to seq length
+            // when line counter == 3, then
+            //   reset line counter to 0
+
+
+                if (inheader == 1) {
+                    if (r == '\n') {
+                        inheader = 0;
+                    }
+                }
+                else if (r == '@') {
+                    ++nseqs;
+                    inheader = 1;
+                    if (seqlen > 0) {
+                        if (seqlen > maxlen) {
+                            maxlen = seqlen;
+                        }
+                        if (seqlen < minlen) {
+                            minlen = seqlen;
+                        }
+                        sum += seqlen;
+                        seqlen = 0;
+                    }
+                }
+                else {
+                    if (r != '\n') {
+                        ++seqlen;
+                    }
+                }
+            }
+
+            // Take care of last seqlen
+            if (seqlen > 0) {
+                if (seqlen > maxlen) {
+                    maxlen = seqlen;
+                }
+                if (seqlen < minlen) {
+                    minlen = seqlen;
+                }
+                sum += seqlen;
+            }
+
+            meanlen = 1.0 * sum / nseqs;
+
+            fprintf(stderr, "%s", "Nseqs\tMin.len\tMax.len\tAvg.len\tFile\n");
+            fprintf(stdout, "%ld\t%ld\t%ld\t%g\t%s\n", nseqs, minlen, maxlen, round(meanlen), basename(fname));
+
+            minlen = INT_MAX;
+            maxlen = INT_MIN;
+            nseqs = 0;
+            seqlen = 0;
+            sum = 0;
+
+            gzclose(zfp);
+
+        }
+    }
+    else {
+        printf("Error: Need input fasta file(s) to process.\n");
+        fprintf(stderr, usage, argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    exit(0);
+
+}
+
