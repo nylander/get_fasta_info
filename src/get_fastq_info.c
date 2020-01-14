4fefb19c179ed03e1e03d8692856e34883998736
nylander <Johan.Nylander@bils.se>
2020-01-14 19:09:17 +0100

    add get_fastq_info

--- src/get_fastq_info.c
+++ src/get_fastq_info.c
@@ -1,7 +1,7 @@
 /*        
 *          File: get_fastq_info.c
 *            By: Johan Nylander
-* Last modified: fre jan 10, 2020  04:22
+* Last modified: Tue Jan 14, 2020  06:53PM
 *   Description: Get min/max/avg sequence length in fastq.
 *                Can read compressed (gzip) files.
 *                Prints to both stdout and stderr.
@@ -9,42 +9,47 @@
 *           Run: get_fastq_info fastq.fq.gz
 */
 
+#include <ctype.h>
+#include <libgen.h>
+#include <limits.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <limits.h>
-#include <libgen.h>
-#include <math.h>
 #include <zlib.h>
 
 int main (int argc, char **argv) {
 
-    long int minlen;
-    long int maxlen;
-    long int sum;
-    long int seqlen;
-    long int nseqs;
-    long int linecounter;
-    float meanlen;
-    char r; // r is the character currently read
-    int inheader;
     char *fname;
+    char r; // r is the character currently read
     extern char *optarg;
     extern int optind;
-    int c, err = 0;
+    float avelen;
+    int c;
+    int err = 0;
+    int linecounter;
+    int verbose = 1;
+    long int maxlen;
+    long int minlen;
+    long int nseqs;
+    long int seqlen;
+    long int sum;
 
-    static char usage[] = "Usage: %s [-h] infile(s)\n";
+    static char usage[] = "\nGet basic summary info about fastq formatted files.\n\nUsage:\n\n %s [-h][-n] infile(s).\n\n  -h is help\n  -n is noverbose\n\n  infile should be in fastq format (gzipped or not).\n\n";
 
     if (argc == 1) {
         fprintf(stderr, usage, argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    while ((c = getopt(argc, argv, "h")) != -1) {
+    while ((c = getopt(argc, argv, "hn")) != -1) {
         switch (c) {
             case 'h':
                 fprintf(stderr, usage, argv[0]);
                 exit(1);
+                break;
+            case 'n':
+                verbose = 0;
                 break;
             case '?':
                 err = 1;
@@ -71,77 +76,50 @@ int main (int argc, char **argv) {
             fname = argv[optind];
             minlen = INT_MAX;
             maxlen = INT_MIN;
-            inheader = 0;
             nseqs = 0;
             seqlen = 0;
             sum = 0;
+            linecounter = 0;
 
             while ((r = gzgetc(zfp)) != EOF) {
-
-            // Initialize line counter to 0;
-            // read character by character;
-            // if the character is a '\n', then
-            //   increment line counter
-            // if the line counter == 1, then
-            //   add to seq length
-            // when line counter == 3, then
-            //   reset line counter to 0
-
-
-                if (inheader == 1) {
-                    if (r == '\n') {
-                        inheader = 0;
-                    }
-                }
-                else if (r == '@') {
-                    ++nseqs;
-                    inheader = 1;
-                    if (seqlen > 0) {
-                        if (seqlen > maxlen) {
-                            maxlen = seqlen;
+                if (r == '\n') {
+                    ++linecounter;
+                    if (linecounter == 4) {
+                        if (seqlen > 0) {
+                            if (seqlen > maxlen) {
+                                maxlen = seqlen;
+                            }
+                            if (seqlen < minlen) {
+                                minlen = seqlen;
+                            }
+                            sum += seqlen;
                         }
-                        if (seqlen < minlen) {
-                            minlen = seqlen;
-                        }
-                        sum += seqlen;
+                        ++nseqs;
+                        linecounter = 0;
                         seqlen = 0;
                     }
                 }
-                else {
-                    if (r != '\n') {
+                if (linecounter == 1) {
+                    if (! isspace(r)) {
                         ++seqlen;
                     }
                 }
             }
 
-            // Take care of last seqlen
-            if (seqlen > 0) {
-                if (seqlen > maxlen) {
-                    maxlen = seqlen;
-                }
-                if (seqlen < minlen) {
-                    minlen = seqlen;
-                }
-                sum += seqlen;
+            avelen = 1.0 * sum / nseqs;
+
+            if (verbose) {
+                fprintf(stderr, "%s", "Nseqs\tMin.len\tMax.len\tAvg.len\tFile\n");
             }
 
-            meanlen = 1.0 * sum / nseqs;
-
-            fprintf(stderr, "%s", "Nseqs\tMin.len\tMax.len\tAvg.len\tFile\n");
-            fprintf(stdout, "%ld\t%ld\t%ld\t%g\t%s\n", nseqs, minlen, maxlen, round(meanlen), basename(fname));
-
-            minlen = INT_MAX;
-            maxlen = INT_MIN;
-            nseqs = 0;
-            seqlen = 0;
-            sum = 0;
+            fprintf(stdout, "%ld\t%ld\t%ld\t%g\t%s\n", nseqs, minlen, maxlen, round(avelen), basename(fname));
 
             gzclose(zfp);
 
         }
     }
     else {
-        printf("Error: Need input fasta file(s) to process.\n");
+        printf("Error: Need input fastq file(s) to process.\n");
         fprintf(stderr, usage, argv[0]);
         exit(EXIT_FAILURE);
     }
