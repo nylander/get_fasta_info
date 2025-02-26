aca73178e8e8936189302e0a46b6d75530225941
nylander <Johan.Nylander@nrm.se>
2025-02-26 17:46:38 +0100

    Minor code changes. Update version and license.

--- src/get_fastq_info.c
+++ src/get_fastq_info.c
@@ -1,14 +1,14 @@
 /*
 *          File: get_fastq_info.c
 *            By: Johan Nylander
-* Last modified: fre apr 08, 2022  03:28
+* Last modified: ons feb 26, 2025  05:44
 *   Description: Get min/max/avg sequence length in fastq.
 *                Can read compressed (gzip) files.
 *                Prints to both stdout and stderr.
 *                Can report average read quality (phred, ASCII_BASE=33) score.
 *       Compile: gcc -Wall -O3 -funroll-loops -o get_fastq_info get_fastq_info.c -lm -lz
 *           Run: get_fastq_info -q fastq.fq.gz
-*       License: Copyright (c) 2019-2022 Johan Nylander
+*       License: Copyright (c) 2019-2025 Johan Nylander
 *                Permission is hereby granted, free of charge, to any person
 *                obtaining a copy of this software and associated documentation
 *                files (the "Software"), to deal in the Software without
@@ -38,50 +38,124 @@
 #include <unistd.h>
 #include <zlib.h>
 
-#define VERSION_STR "2.4.0"
+#define VERSION_STR "2.4.1"
 
-int main (int argc, char **argv) {
+void print_usage(const char *prog_name) {
+    fprintf(stderr, "\n%s v%s\n", prog_name, VERSION_STR);
+    fprintf(stderr, "\nGet basic summary info about fastq formatted files.\n\n");
+    fprintf(stderr, "Usage:\n\n %s [options] infile(s).\n\n", prog_name);
+    fprintf(stderr, "Options:\n\n");
+    fprintf(stderr, " -h  help\n");
+    fprintf(stderr, " -V  version\n");
+    fprintf(stderr, " -n  noverbose\n");
+    fprintf(stderr, " -q  show avg. read qual (ASCII_BASE=33)\n");
+    fprintf(stderr, " infile should be in fastq format (gzipped or not).\n\n");
+}
 
-    char *fname;
-    char *res;
+void process_file(const char *fname, int verbose, int quality, int fullpath) {
     char buf[PATH_MAX];
     char r; // r is the character currently read
-    extern char *optarg;
-    extern int optind;
-    float avelen = 0.0f;
-    float avequal = 0.0f;
+    gzFile zfp = gzopen(fname, "rb");
+    if (zfp == NULL) {
+        perror("Error: failed in opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    long int minlen = INT_MAX;
+    long int maxlen = INT_MIN;
+    long int nseqs = 0;
+    long int seqlen = 0;
+    long int sum = 0;
+    long int qsum = 0;
+    int linecounter = 0;
+    int sqsum = 0;
+
+    while ((r = gzgetc(zfp)) != EOF) {
+        if (r == '\n') {
+            ++linecounter;
+            if (linecounter == 4) {
+                if (seqlen > 0) {
+                    if (seqlen > maxlen) {
+                        maxlen = seqlen;
+                    }
+                    if (seqlen < minlen) {
+                        minlen = seqlen;
+                    }
+                    sum += seqlen;
+                }
+                if (quality && sqsum > 0) {
+                    qsum += round(sqsum / (float)seqlen);
+                    sqsum = 0;
+                }
+                ++nseqs;
+                linecounter = 0;
+                seqlen = 0;
+            }
+        }
+        else if (linecounter == 1 && !isspace(r)) {
+            ++seqlen;
+        }
+        else if (linecounter == 3 && quality && !isspace(r)) {
+            sqsum += r - 33; // ASCII_BASE=33
+        }
+    }
+
+    float avelen = 1.0 * sum / nseqs;
+    float avequal = quality ? 1.0 * qsum / nseqs : 0;
+
+    if (verbose) {
+        if (quality) {
+            fprintf(stderr, "Nseqs\tMin.len\tMax.len\tAvg.len\tAvg.qual\tFile\n");
+        }
+        else {
+            fprintf(stderr, "Nseqs\tMin.len\tMax.len\tAvg.len\tFile\n");
+        }
+    }
+
+    if (quality) {
+        fprintf(stdout, "%ld\t%ld\t%ld\t%g\t%g\t", nseqs, minlen, maxlen, round(avelen), round(avequal));
+    }
+    else {
+        fprintf(stdout, "%ld\t%ld\t%ld\t%g\t", nseqs, minlen, maxlen, round(avelen));
+    }
+
+    if (fullpath) {
+        char *res = realpath(fname, buf);
+        if (res) {
+            fprintf(stdout, "%s\n", buf);
+        }
+        else {
+            fprintf(stdout, "Error: Failed getting realpath of infile %s.\n", fname);
+            exit(EXIT_FAILURE);
+        }
+    }
+    else {
+        fprintf(stdout, "%s\n", basename((char*)fname));
+    }
+
+    gzclose(zfp);
+}
+
+int main(int argc, char **argv) {
     int c;
     int err = 0;
     int fullpath = 0;
-    int linecounter;
-    int q;
-    int quality = 0;
-    int sqsum = 0;
     int verbose = 1;
-    long int maxlen;
-    long int minlen;
-    long int nseqs;
-    long int qsum = 0;
-    long int seqlen;
-    long int sum;
-
-    static char usage[] = "\nGet basic summary info about fastq formatted files.\n\nUsage:\n\n %s [-h][-n][-q][-V] infile(s).\n\n  -h help\n  -n noverbose\n  -V version\n  -q show avg. read qual (ASCII_BASE=33)\n\n\n  infile should be in fastq format (gzipped or not).\n\n";
+    int quality = 0;
 
     if (argc == 1) {
-        fprintf(stderr, usage, argv[0]);
+        print_usage(argv[0]);
         exit(EXIT_FAILURE);
     }
 
     while ((c = getopt(argc, argv, "hVpnq")) != -1) {
         switch (c) {
             case 'h':
-                fprintf(stderr, usage, argv[0]);
+                print_usage(argv[0]);
                 exit(EXIT_SUCCESS);
-                break;
             case 'V':
                 fprintf(stdout, "%s\n", VERSION_STR);
                 exit(EXIT_SUCCESS);
-                break;
             case 'p':
                 fullpath = 1;
                 break;
@@ -98,117 +172,19 @@ int main (int argc, char **argv) {
     }
 
     if (err) {
-        fprintf(stderr, usage, argv[0]);
+        print_usage(argv[0]);
         exit(EXIT_FAILURE);
     }
 
     if (optind < argc) {
-
         for (; optind < argc; optind++) {
-
-            gzFile zfp = gzopen(argv[optind], "rb");
-
-            if (zfp == NULL) {
-                perror("Error: failed in opening file");
-                exit(EXIT_FAILURE);
-            }
-
-            fname = argv[optind];
-            minlen = INT_MAX;
-            maxlen = INT_MIN;
-            nseqs = 0;
-            seqlen = 0;
-            sum = 0;
-            linecounter = 0;
-
-            while ((r = gzgetc(zfp)) != EOF) {
-                if (r == '\n') {
-                    ++linecounter;
-                    if (linecounter == 4) {
-                        if (seqlen > 0) {
-                            if (seqlen > maxlen) {
-                                maxlen = seqlen;
-                            }
-                            if (seqlen < minlen) {
-                                minlen = seqlen;
-                            }
-                            sum += seqlen;
-                        }
-                        if (quality == 1) {
-                            if (sqsum > 0) {
-                                qsum += round( sqsum / seqlen);
-                            }
-                            sqsum = 0;
-                        }
-                        ++nseqs;
-                        linecounter = 0;
-                        seqlen = 0;
-                    }
-                }
-                if (linecounter == 1) {
-                    if (! isspace(r)) {
-                        ++seqlen;
-                    }
-                }
-                else if (linecounter == 3) {
-                    if (quality == 1) {
-                        if (! isspace(r)) {
-                            q = r - 33; // ASCII_BASE=33
-                            sqsum += q;
-                        }
-                    }
-                }
-            }
-
-            avelen = 1.0 * sum / nseqs;
-
-            if (quality == 1) {
-                avequal = 1.0 * qsum / nseqs;
-            }
-
-            if (verbose) {
-                if (quality == 1) {
-                    fprintf(stderr, "%s", "Nseqs\tMin.len\tMax.len\tAvg.len\tAvg.qual\tFile\n");
-                }
-                else {
-                    fprintf(stderr, "%s", "Nseqs\tMin.len\tMax.len\tAvg.len\tFile\n");
-                }
-            }
-
-            if (quality == 1) {
-                //fprintf(stdout, "%ld\t%ld\t%ld\t%g\t%g\t%s\n", nseqs, minlen, maxlen, round(avelen), round(avequal), basename(fname));
-                fprintf(stdout, "%ld\t%ld\t%ld\t%g\t%g\t", nseqs, minlen, maxlen, round(avelen), round(avequal));
-            }
-            else {
-                //fprintf(stdout, "%ld\t%ld\t%ld\t%g\t%s\n", nseqs, minlen, maxlen, round(avelen), basename(fname));
-                fprintf(stdout, "%ld\t%ld\t%ld\t%g\t", nseqs, minlen, maxlen, round(avelen));
-            }
-
-            if (fullpath == 1) {
-                res = realpath(fname, buf);
-                if (res) {
-                    fprintf(stdout, "%s\n", buf);
-                }
-                else {
-                    fprintf(stdout, "Error: Failed getting realpath of infile %s.\n", fname);
-                    exit(EXIT_FAILURE);
-                }
-            }
-            else {
-                fprintf(stdout,"%s\n", basename(fname));
-            }
-
-            gzclose(zfp);
-
+            process_file(argv[optind], verbose, quality, fullpath);
         }
-    }
-    else {
-        printf("Error: Need input fastq file(s) to process.\n");
-        fprintf(stderr, usage, argv[0]);
+    } else {
+        fprintf(stderr, "Error: Need input fastq file(s) to process.\n");
+        print_usage(argv[0]);
         exit(EXIT_FAILURE);
     }
 
     return EXIT_SUCCESS;
-
 }
-
